db1daaea92d0a9f6b2f987092f9eee4dbbe4823a
pierreozoux <pierre@ozoux.net>
2016-02-25 13:05:10 +0000

    quick fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 while true
 do
   START=`date +%s`
-  rsync -avz --delete -e 'ssh -p$PORT' root@$SERVER:/share/ /backups
+  rsync -avz --delete -e "ssh -p$PORT" root@$SERVER:/share/ /backups
   sleep $((3600 - `date +%s` + $START))
 done
